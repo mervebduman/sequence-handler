0e680e139141825e9af643ffcdac64e21472f70a
mervebduman <merveb.duman@hotmail.com>
2024-05-08 16:13:55 +0200

    refactor: code in def

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -1,14 +1,15 @@
 import sqlite3
 
-conn = sqlite3.connect('seq_handler.db')
-crsr = conn.cursor()
- 
-# create table
-sql_command = """CREATE TABLE seq ( 
-id TEXT, 
-seq TEXT
-);"""
- 
-crsr.execute(sql_command)
+def create_db():
+    conn = sqlite3.connect('seq_handler.db')
+    crsr = conn.cursor()
+    
+    # create table
+    sql_command = """CREATE TABLE seq ( 
+    id TEXT, 
+    seq TEXT
+    );"""
+    
+    crsr.execute(sql_command)
 
-conn.close()
+    conn.close()
